d5da6bba27a49d3fa2291ac3759b203afb1e1457
Yusuke Mabuchi <yusukemabuchi@mbp13.local>
2020-11-19 06:56:34 +0900

    Add select

--- src/templates/RotoryOn.tsx
+++ src/templates/RotoryOn.tsx
@@ -1,10 +1,34 @@
+// React
 import React, { FC } from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+// Material-UI
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core/styles';
+import {
+  CssBaseline,
+  Container,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  ThemeProvider,
+} from '@material-ui/core';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       marginTop: 80,
+    },
+    title: {
+      marginBottom: 24,
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 320,
     },
   }),
 );
@@ -14,7 +38,46 @@ const RotoryOn: FC = () => {
 
   return (
     <div className={classes.root}>
-      <p>This is RotoryOn.</p>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Typography
+          className={classes.title}
+          variant="h4"
+          component="h2"
+          align="center"
+        >
+          抽選！
+        </Typography>
+        <FormControl
+          className={classes.formControl}
+        >
+          <InputLabel
+            id="rotory-type"
+          >
+            抽選する賞を選んでください
+          </InputLabel>
+          <Select
+            labelId="rotory-type"
+            id="select-gift"
+          >
+            <MenuItem
+              value={0}
+            >
+              特賞
+            </MenuItem>
+            <MenuItem
+              value={1}
+            >
+              1等
+            </MenuItem>
+            <MenuItem
+              value={2}
+            >
+              2等
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Container>
     </div>
   );
 };
