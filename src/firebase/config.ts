f08227f314f68c6bd52a2d54332469e97df006d4
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-11-20 16:35:46 +0900

    Change gitignore setting

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyARsk-FxydrNAMmI92RrCnuhVuFYj-8R6s",
+  authDomain: "rotory-on.firebaseapp.com",
+  databaseURL: "https://rotory-on.firebaseio.com",
+  projectId: "rotory-on",
+  storageBucket: "rotory-on.appspot.com",
+  messagingSenderId: "881169189785",
+  appId: "1:881169189785:web:5075d75012591baee7a108",
+  measurementId: "G-8Y6TQFQ628"
+};
